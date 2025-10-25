0dc063a830cce244a1d0db4c514d6eb1ec89a0d0
Явор Налбантов <javor.nalbantov@gmail.com>
2025-10-25 11:47:04 +0300

    ok

--- Models/QuizMVC.cs
+++ Models/QuizMVC.cs
@@ -6,6 +6,226 @@ namespace QUIZ_APP.Models
     {
 
         //************************************************************************************
+        public void AddMVC()
+        {
+
+
+            this.QuizTitle = "Quiz MVC";
+
+            this.Add
+                (
+                    "Which annotation is used to define a controller in Spring MVC?",
+                    new OptionsInQuestion()
+                    {
+                        Letter = enum_LetterOption.A,
+                        QuestionText = "@Component",
+                        Correct = true
+                    },
+                      new OptionsInQuestion()
+                      {
+                          Letter = enum_LetterOption.B,
+                          QuestionText = "@Controller",
+                          Correct = false
+                      },
+                        new OptionsInQuestion()
+                        {
+                            Letter = enum_LetterOption.C,
+                            QuestionText = "@RestService",
+                            Correct = false
+                        },
+                         new OptionsInQuestion()
+                         {
+                             Letter = enum_LetterOption.D,
+                             QuestionText = "@MVCController",
+                             Correct = false
+                         }
+
+                );
+            this.Add
+                (
+                    "Which method in a controller returns a View in ASP.NET Core MVC?",
+                    new OptionsInQuestion()
+                    {
+                        Letter = enum_LetterOption.A,
+                        Correct = true,
+                        QuestionText = "Display",
+                    },
+                     new OptionsInQuestion()
+                     {
+                         Letter = enum_LetterOption.B,
+                         Correct = false,
+                         QuestionText = "Show",
+                     },
+                      new OptionsInQuestion()
+                      {
+                          Letter = enum_LetterOption.C,
+                          Correct = false,
+                          QuestionText = "View",
+                      },
+                       new OptionsInQuestion()
+                       {
+                           Letter = enum_LetterOption.D,
+                           Correct = false,
+                           QuestionText = "Render",
+                       }
+                );
+            this.Add
+                (
+                    "Which method is used to define a GET route in Express?",
+                    new OptionsInQuestion()
+                    {
+                        Letter = enum_LetterOption.A,
+                        Correct = true,
+                        QuestionText = "app.route()"
+                    },
+                     new OptionsInQuestion()
+                     {
+                         Letter = enum_LetterOption.B,
+                         Correct = false,
+                         QuestionText = "app.get()"
+                     },
+                      new OptionsInQuestion()
+                      {
+                          Letter = enum_LetterOption.C,
+                          Correct = false,
+                          QuestionText = "express.get()"
+                      },
+                       new OptionsInQuestion()
+                       {
+                           Letter = enum_LetterOption.D,
+                           Correct = false,
+                           QuestionText = "router.create()"
+                       }
+
+                );
+
+            this.Add
+                (
+                    "In MVC architecture, what does the 'C' stand for?",
+                    new OptionsInQuestion()
+                    {
+                        Letter = enum_LetterOption.A,
+                        Correct = true,
+                        QuestionText = "Code",
+                    },
+                    new OptionsInQuestion()
+                    {
+                        Letter = enum_LetterOption.B,
+                        Correct = false,
+                        QuestionText = "Controller",
+                    },
+                    new OptionsInQuestion()
+                    {
+                        Letter = enum_LetterOption.C,
+                        Correct = false,
+                        QuestionText = "Class",
+                    },
+                    new OptionsInQuestion()
+                    {
+                        Letter = enum_LetterOption.D,
+                        Correct = false,
+                        QuestionText = "Content",
+                    }
+
+                );
+            this.Add
+                (
+                    "Which part of MVC is responsible for handling user input?",
+                    new OptionsInQuestion()
+                    {
+                        Letter = enum_LetterOption.A,
+                        Correct = true,
+                        QuestionText = "Model",
+                    },
+                    new OptionsInQuestion()
+                    {
+                        Letter = enum_LetterOption.B,
+                        Correct = false,
+                        QuestionText = "View",
+                    },
+                    new OptionsInQuestion()
+                    {
+                        Letter = enum_LetterOption.C,
+                        Correct = false,
+                        QuestionText = "Controller",
+                    },
+                    new OptionsInQuestion()
+                    {
+                        Letter = enum_LetterOption.D,
+                        Correct = false,
+                        QuestionText = "Template",
+                    }
+
+                );
+        }
+        //************************************************************************************
+        public void AddJS()
+        {
+
+
+            this.QuizTitle = "Quiz JS";
+
+            this.Add
+                (
+                    "Do you have any plans to add design Pattern , Context concept",
+                    new OptionsInQuestion()
+                    {
+                        Letter = enum_LetterOption.A,
+                        QuestionText = "yes",
+                        Correct = true
+                    },
+                      new OptionsInQuestion()
+                      {
+                          Letter = enum_LetterOption.B,
+                          QuestionText = "No",
+                          Correct = false
+                      },
+                        new OptionsInQuestion()
+                        {
+                            Letter = enum_LetterOption.C,
+                            QuestionText = "maybe",
+                            Correct = false
+                        },
+                         new OptionsInQuestion()
+                         {
+                             Letter = enum_LetterOption.D,
+                             QuestionText = ":)",
+                             Correct = false
+                         }
+
+                );
+            this.Add
+                (
+                    "w3schools is...",
+                    new OptionsInQuestion()
+                    {
+                        Letter = enum_LetterOption.A,
+                        Correct = true,
+                        QuestionText = "+++ learn by writing code",
+                    },
+                     new OptionsInQuestion()
+                     {
+                         Letter = enum_LetterOption.B,
+                         Correct = false,
+                         QuestionText = "- outdated",
+                     },
+                      new OptionsInQuestion()
+                      {
+                          Letter = enum_LetterOption.C,
+                          Correct = false,
+                          QuestionText = "- not comprehensive",
+                      },
+                       new OptionsInQuestion()
+                       {
+                           Letter = enum_LetterOption.D,
+                           Correct = false,
+                           QuestionText = "- not very good explaining async fundamentals",
+                       }
+                );
+        }
+
+
+        //************************************************************************************
         public string QuizTitle { get; set; }
         //************************************************************************************
         public int QuizIndex { get; set; }
